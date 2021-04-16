26df2928f01a85513dd0b27393a076cf5ba62f41
Cully Mason <cully@hey.com>
2021-04-15 20:33:09 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen space-y-6 min-w-screen bg-gradient-to-br from-gray-50 to-white">
+      <div class="w-11/12 p-px shadow-inner 2xl:w-1/3 bg-gradient-to-br from-gray-100 to-white rounded-2xl">
+        <div class="flex flex-col p-4 m-px bg-white rounded-xl">
+            <h1 class="mb-8 text-4xl text-gray-900">Cully's Domains</h1>
+            <ul class="space-y-5 font-mono text-gray-700">
+                <li>altdelight.com</li>
+                <li>cinch.run</li>
+                <li>drinkdrop.app</li>
+                <li>foodfacade.com</li>
+                <li>nubs.in</li>
+                <li>nuzzle.in</li>
+                <li>parse.one</li>
+                <li>pkg.tools</li>
+                <li>silo.pw</li>
+                <li>tablewhiskey.glass</li>
+                <li>toddy.house</li>
+                <li>triage.support</li>
+                <li>via.pub</li>
+                <li>volii.app</li>
+            </ul>
+        </div>
+      </div>
+      <div>
+            Interested? Email cully@hey.com
+        </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
